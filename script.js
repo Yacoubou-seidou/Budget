017802b95745efe57b61e5a75f44e1c94234b770
Yacos <84572891+Yacoubou-seidou@users.noreply.github.com>
2022-12-02 08:23:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,114 +1,193 @@
-// les variables elements du DOM
-const moneyEl = document.getElementById("money-el");
-const btnMoney = document.getElementById("btn-money");
-const amountName = document.getElementById("amount-name");
-const amountPrice = document.getElementById("amount-price");
-const addAmount = document.getElementById("add-amount");
-const budgetEl = document.getElementById("budget-el");
-const expensesEl = document.getElementById("expenses-el");
-const balanceEl = document.getElementById("balance-el");
-const mesDepense = document.getElementById("mes-depense");
-let argent = 0; //collecter dans la variable ensuite affecter au budget
-let depenses = 0; //collecter ensuite affecter
-let defo = 0; //collecter ensuite affecter
-//function definir un budget
-function addBudget() {
-  if (moneyEl.value <= 0) {
-    //si un budget n'est pas defini
-    alert("Error");
-  } else {
-    //s'il est defini
-    argent = moneyEl.value;
-    budgetEl.textContent = argent;
-  }
-  //reinitialiser l'input
-  moneyEl.value = "";
-}
-// function definir une depense
-function addDepenses() {
-  //si y'a une depense
-  if (
-    amountPrice.valueAsNumber > 0 &&
-    amountName.value != "" &&
-    budgetEl.textContent > 0
-  ) {
-    //affecter la depense
-    depenses += amountPrice.valueAsNumber;
-    expensesEl.textContent = depenses;
-    balanceEl.textContent = argent - depenses;
-    defo = amountPrice.valueAsNumber;
-    //ajouter la depense dans mesdepenses
-    mesDepense.innerHTML += `<div class="lesdepens">
-            <h2 class="titre les-red">${amountName.value}</h2>
-            <h2 class="titre depla les-red">${defo}</h2><span class="dollarr">Fcfa</span>
-            <h2 class="titre les-red btn-par">
-              <button class="icone edit-btn">
-                <i class="fa-solid fa-pen-to-square"></i>
-              </button>
-              <button class="icone delete-btn" >
-                <i class="fa-solid fa-trash deleted"></i>
-              </button>
-            </h2>
-          </div>`;
+const budget = document.getElementById("budget");
+const addBudget = document.getElementById("addBudget");
+const bien = document.getElementById("bien");
+const credit = document.getElementById("credit");
+const solde = document.getElementById("solde");
+const expenseId = document.getElementById("expenseId");
+const expenseName = document.getElementById("expenseName");
+const expenseValue = document.getElementById("expenseValue");
+const addExpense = document.getElementById("addExpense");
+const expenseTitle = document.getElementById("expenseTitle");
+const expenseMontant = document.getElementById("expenseMontant");
+const expenseOperation = document.getElementById("expenseOperation");
+const notif = document.getElementById("notif");
+const content = document.getElementById("content");
+
+let globalBudget = 0;
+let globalExpense = [];
+let message = type = "";
+
+window.addEventListener("load", function () {
+    calculate();
+});
+
+budget.addEventListener("change", () => {
+    if (budget.value < 0) {
+        budget.value = "";
+        message = "Votre buget doit être positive";
+        type = "info";
+        notification(notif, message, type);
+    }
+});
+
+addBudget.addEventListener("click", e => {
+    e.preventDefault();
+    let budgetValue = budget.value;
+    if (budgetValue == "") {
+        message = "Vous devez en amont saisir une valeur avant de sauvegarder";
+        type = "danger";
+        notification(notif, message, type);
+    } else {
+        message = "Votre budget a été ajouté avec success";
+        type = "success";
+        notification(notif, message, type);
+        globalBudget += parseInt(budgetValue);
+        budget.value = "";
+    }
+    calculate();
+});
+
+expenseName.addEventListener("change", () => {
+    if (expenseName.value.length < 2) {
+        expenseName.value = "";
+        message =
+            "La designation de votre depense doit comporter au moins 2 caracteres";
+        type = "info";
+        notification(notif, message, type);
+    }
+});
+
+expenseValue.addEventListener("change", () => {
+    if (expenseValue.value < 0) {
+        expenseValue.value = "";
+        message = "Le montant de votre depense doit être positive";
+        type = "info";
+        notification(notif, message, type);
+    }
+});
+
+addExpense.addEventListener("click", e => {
+    e.preventDefault();
+    let expense = {
+        name: expenseName.value,
+        montant: expenseValue.value,
+    };
+    if (expense.name == "" && expense.montant == "") {
+        message =
+            "Vous devez en amont saisir une designation et une valeur pour la depense avant de sauvegarder";
+        type = "danger";
+        notification(notif, message, type);
+    } else {
+        message = "Votre depense a été ajouté avec success";
+        type = "success";
+        notification(notif, message, type);
+        const check = InArray (globalExpense, expenseName.value);
+        if (expenseId.value != '' || check != '-1') {
+            const index = (check != '-1') ? check : expenseId.value;
+            if(check == '-1') globalExpense[index].name = expenseName.value;
+            if(expenseId.value != ''){
+                if(expenseId.value == check){
+                    globalExpense[index].montant = expenseValue.value;
+                } else {
+                    globalExpense[index].montant =  parseInt(globalExpense[index].montant) + parseInt(expenseValue.value);
+                    if(check != '-1') {
+                        globalExpense.splice(expenseId.value, 1)
+                    }
+                }
+            } else {
+                globalExpense[index].montant =  parseInt(globalExpense[index].montant) + parseInt(expenseValue.value);
+            }
+            expenseId.value = "";
+        } else {
+            globalExpense.push(expense);
+        }
+        expenseName.value = expenseValue.value = "";
+    }
+    calculate();
+});
+
+function calculate() {
+    let expense = 0;
+    bien.innerHTML = `${numberWithEspace(globalBudget)} CFA`;
+    let i = 0;
+    let tr = "";
+    for (global of globalExpense) {
+        expense += parseInt(global.montant);
+        let title = global.name;
+        let montant = `${numberWithEspace(global.montant)} CFA`;
+        let operate = `
+            <button type="button" data-el="${i}" class="btn btn-light edit"><i class="fa-solid fa-pen-to-square text-info"></i></button>
+            <button type="button" data-el="${i}" class="btn btn-light delete"><i class="fa-solid fa-trash text-danger"></i></button>
+        `;
+
+        tr += `
+            <tr>
+                <td class="col-md-4 text-center" >${title}</td>
+                <td class="col-md-4 text-center">${montant}</d>
+                <td class="col-md-4 text-center">${operate}</td>
+            </tr>
+        `;
+        i++;
+    }
+
+    content.innerHTML = tr;
+    credit.innerHTML = `${numberWithEspace(expense)} CFA`;
+    let diff = globalBudget - expense;
+    
+    if (diff < 0) {
+        solde.innerHTML = `${numberWithEspace(diff) * -1} CFA`;
+        solde.classList.remove("text-success");
+        solde.classList.add("text-danger");
+    } else {
+        solde.innerHTML = `${numberWithEspace(diff)} CFA`;
+        solde.classList.add("text-success");
+        solde.classList.remove("text-danger");
+    }
     listen();
-    //reinitialiser les input
-    amountPrice.value = "";
-    amountName.value = "";
-  } else {
-    //sinon erreur
-    alert("error");
-  }
-  //mettre ajour les couleurs
-  checkBalance();
 }
-//check balance etat pour afecter couleur
-function checkBalance() {
-  if (balanceEl.textContent < 0) {
-    balanceEl.style.color = "red";
-  } else {
-    balanceEl.style.color = "green";
-  }
+
+function notification(notif, message, type) {
+    notif.innerHTML = `<div class="alert alert-${type} mt-2" role="alert">${message}</div>`;
+
+    setInterval(() => {
+        notif.innerHTML = "";
+    }, 5000);
 }
+
+function numberWithEspace(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}
+
 // function editer la depense
 function listen() {
-  //modifier
-  mesDepense.querySelectorAll(".edit-btn").forEach((btn) => {
-    btn.addEventListener("click", function () {
-      console.log("Hello World");
-      //selection le parent exact du button
-      let removeEl = this.parentNode.parentNode;
-      console.log(this.parentNode);
-      //prendre ses enfants pour collecter les données et les envoyer dans les inputs
-      amountName.value = removeEl.children[0].textContent;
-      amountPrice.value = removeEl.children[1].textContent;
-      depenses = depenses - amountPrice.value;
-      console.log(removeEl.children[0]);
-      console.log(removeEl.children[1]);
-      //affecter les numbres
-      expensesEl.textContent = depenses;
-      balanceEl.textContent = argent - depenses;
-      //supprimer la div de la section
-      mesDepense.removeChild(removeEl);
-      //mettre a jours les couleurs
-      checkBalance();
+    //Editer
+    content.querySelectorAll(".edit").forEach((edit) => {
+        edit.addEventListener("click", () => {
+            const index = edit.getAttribute('data-el');
+            expenseId.value = index;
+            expenseName.value = globalExpense[index].name;
+            expenseValue.value = globalExpense[index].montant;
+        });
     });
-  });
-  //supprimer
-  mesDepense.querySelectorAll(".delete-btn").forEach((btn) => {
-    btn.addEventListener("click", function () {
-      console.log(this);
-      //prender le- parent exact
-      let removeEl = this.parentNode.parentNode;
-      console.log(removeEl);
-      //retirer la div
-      mesDepense.removeChild(removeEl);
-      //affecter les numbre
-      depenses = depenses - removeEl.children[1].textContent;
-      expensesEl.textContent = depenses;
-      balanceEl.textContent = argent - depenses;
-      //mettre a jours les couleurs
-      checkBalance();
+    //Supprimer
+    content.querySelectorAll(".delete").forEach((edit) => {
+        edit.addEventListener("click", () => {
+            expenseId.value = '';
+            const index = edit.getAttribute('data-el');
+            globalExpense.splice(index, 1);
+            calculate()
+        });
     });
-  });
 }
-listen();
+
+function InArray (array, elem){
+    let i = 0;
+    for(arr of array){
+        if(arr.name.toLowerCase() == elem.toLowerCase()){
+            return i;
+        }
+        i++;
+    }
+    return -1;
+}
+
